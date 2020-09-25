71822f7b01c4d5a2818d831552e9533d53e92150
gasperpb <achillespb@hotmail.com>
2020-09-25 02:41:48 -0300

    Button copy

--- js/app.js
+++ js/app.js
@@ -2,4 +2,19 @@ function clicar() {
     let num = document.getElementById('numero').value
     let gera = document.getElementById('mensagem')
     gera.innerHTML = 'https://api.whatsapp.com/send?phone=55' + num
+
+
+
+
+
+}
+
+function copiarTexto() {
+    //captura o elemento input
+    const inputTest = document.getElementById('mensagem')
+        //seleciona todo o texto do elemento
+    inputTest.innerText()
+        //executa o comando copy
+        //aqui é feito o ato de copiar para a area de trabalho com base na seleção
+    document.execCommand('copy')
 }
