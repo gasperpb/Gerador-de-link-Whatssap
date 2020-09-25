5c6cbd770e6aeaf5246ae7136c98409841fc4212
gasperpb <achillespb@hotmail.com>
2020-09-25 01:26:40 -0300

    Funcionando, falta o estilo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function clicar() {
-    let num = document.getElementById('numero').value;
+    let num = document.getElementById('numero').value
     let gera = document.getElementById('mensagem')
-    gera.innerText = 'https://api.whatsapp.com/send?phone=55' + num
+    gera.innerHTML = 'https://api.whatsapp.com/send?phone=55' + num
 }
